d60cac5051eb287c5fa111a0f4f0f25432c3cc19
Kevin Zhang <kevin.zhang.13499@gmail.com>
2015-04-30 21:45:35 -0700

    Set up environment and create basic PID Function

---
+++ Main.ino
@@ -0,0 +1,31 @@
+int leftServoPin = 1;  //port number for left motor
+int rightServoPin = 2; //port numbersfor right motor
+int pulse = 1500; //delay in microseconds
+
+void setup()
+{
+  pinMode(leftServoPin, OUTPUT);
+  pinMode(rightServoPin, OUTPUT);
+  Serial.begin(9600);
+
+void loop() {
+  double desiredTilt = 0;
+  double currentTilt = getCurrentTilt();
+  double tiltRate = getCurrentTiltRate();
+  //constants are random guesses from robot tuning this year
+  double kP = 1;
+  double kI = 0;
+  double kD = 0.5; 
+  double torq = pid(currentTilt-desiredTilt, 0, currentTilt + currentTiltRate, kP, kI, kD);
+}
+
+double getCurrentTilt(){
+  //get current tilt from gyro
+}
+
+double getCurrentTiltRate(){
+  //get the rate possibly through gyro, or we can manually calculate it
+}
+double pid(error, integral, derivative, Kp, Ki, Kd) {
+  return -(error * Kp + integral * Ki + derivative * Kd);
+}
